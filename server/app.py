27acbe43227777d9d8fe841e93ba369fddeda354
Harry San Juan <harrycsanjuan@gmail.com>
2024-07-23 21:06:06 -0400

    complete lab

--- server/app.py
+++ server/app.py
@@ -21,7 +21,43 @@ def index():
     return make_response(body, 200)
 
 # Add views here
+@app.route('/earthquakes/<int:id>')
+def earthquake_by_id(id):
+    earthquake = Earthquake.query.filter_by(id=id).first()
+    
+    if earthquake:
+        body = {
+            'id': earthquake.id,
+            'location': earthquake.location,
+            'magnitude': earthquake.magnitude,
+            'year': earthquake.year
+        }
+        status = 200
+    else:
+        body = {'message': f'Earthquake {id} not found.'}
+        status = 404
 
+    return make_response(body, status)
+
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def earthquakes_by_magnitude(magnitude):
+    earthquakes = []
+
+    for quake in Earthquake.query.filter(Earthquake.magnitude >= magnitude).all():
+        quake_dict = {
+            'id': quake.id,
+            'location': quake.location,
+            'magnitude': quake.magnitude,
+            'year': quake.year
+        }
+        earthquakes.append(quake_dict)
+
+    body = {
+        'count': len(earthquakes),
+        'quakes': earthquakes
+    }
+
+    return make_response(body, 200)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
